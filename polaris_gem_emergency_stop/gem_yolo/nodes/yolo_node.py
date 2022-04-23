01e4a3ca63d835bb949eae57ea26093b7269dd07
danielz <31590926+danielz02@users.noreply.github.com>
2022-04-23 01:30:29 -0500

    Add launch file for Yolo module

--- polaris_gem_emergency_stop/gem_yolo/nodes/yolo_node.py
+++ polaris_gem_emergency_stop/gem_yolo/nodes/yolo_node.py
@@ -1,7 +1,6 @@
 import sys
-from typing import Optional
-import cv2
 import torch
+from torch import hub
 import rospy
 import numpy as np
 from cv_bridge import CvBridgeError, CvBridge
@@ -9,40 +8,43 @@ from sensor_msgs.msg import Image
 
 
 class YoloDetector:
-    def __init__(self, pub_annotated_img: Optional[rospy.Publisher] = None,):
+    def __init__(self, pub_annotated_img: rospy.Publisher, src_dir):
+        self.detection = None
         self.bridge = CvBridge()
         self.pub = pub_annotated_img
 
-        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+        self.model = torch.hub.load(src_dir, model="yolov5x6", source="local")
 
     def img_callback(self, data: Image):
         try:
-            rospy.loginfo("Image Callback")
             cv_image = self.bridge.imgmsg_to_cv2(data, "rgb8")
             cv_image = np.asarray(cv_image)
             yolo_detection = self.model(cv_image, size=data.height)
+            self.detection = yolo_detection
             bbox_img = yolo_detection.render()
-            bbox_img = cv2.resize(*bbox_img, (data.width, data.height))
 
-            self.pub.publish(self.bridge.cv2_to_imgmsg(bbox_img, "rgb8"))
+            rospy.loginfo(yolo_detection.print())
+            self.pub.publish(self.bridge.cv2_to_imgmsg(*bbox_img, "rgb8"))
         except CvBridgeError as e:
             rospy.logerr(e)
 
 
 def main(argv):
     rospy.init_node('yolo_node', anonymous=True)
+    is_simulator = rospy.get_param("~is_simulator", False)
+    src_dir = rospy.get_param("~models_path")
 
     pub_annotated_image = rospy.Publisher("annotated_image/yolo", Image, queue_size=1)
-    net = YoloDetector(pub_annotated_image)
+    net = YoloDetector(pub_annotated_image, src_dir)
 
     _ = rospy.Subscriber(
-        "/mako_1/mako_1/image_raw",  # /gem/front_single_camera/image_raw
+        "/mako_1/mako_1/image_raw" if not is_simulator else "/gem/front_single_camera/image_raw",
         Image, net.img_callback, queue_size=1
     )
     try:
         rospy.spin()
     except KeyboardInterrupt:
-        rospy.loginfo("Shutting down LaneNet node")
+        rospy.loginfo("Shutting down Yolo node")
 
 
 if __name__ == '__main__':
